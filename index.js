64ccb8d8cf113a9f05fb8844f743e74e274de3a5
hassanbaryal <h.baryal2@gmail.com>
2022-10-27 16:26:54 -0400

    Created merge sort algorithm

--- index.js
+++ index.js
@@ -11,3 +11,28 @@ function createRandomArray (size) {
 
   return array;
 };
+
+function mergeSort (arr) {
+  if (arr.length < 2) {
+    return arr;
+  } else {
+    let left = mergeSort(arr.slice(0, arr.length / 2));
+    let right = mergeSort(arr.slice(arr.length / 2));
+    let sorted = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+    while ((leftIndex < left.length) && (rightIndex < right.length)) {
+      if(left[leftIndex] < right[rightIndex]){
+        sorted.push(left[leftIndex++])
+      } else {
+        sorted.push(right[rightIndex++]);
+      }
+    }
+    if (leftIndex >= left.length) {
+      sorted = sorted.concat(right.slice(rightIndex))
+    } else {
+      sorted = sorted.concat(left.slice(leftIndex));
+    }
+    return sorted;
+  }
+};
